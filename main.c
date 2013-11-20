2612413d748f439676ff23f76c984fa4af1f6ad3
cosc301 <cosc301@csvm.(none)>
2013-11-20 14:45:18 -0500

    IT LIIIVVVESSSS

--- main.c
+++ main.c
@@ -114,6 +114,7 @@ void * worker(void * arg) {
 		pthread_mutex_lock(&mucheck);
 		while (thread_count==0 && tail==NULL) { //if there is nothing in the queue, wait
 			pthread_cond_wait(&thread, &mucheck);
+			if(!still_running) return NULL;
 		}
 		thread_count++;
     		struct work_queue_item * temp = remove_tail(); //remove tail for connection info
@@ -136,24 +137,35 @@ void * worker(void * arg) {
             		fprintf(log_file, "%s:%d  %s  'GET %s'  %d %d\n", inet_ntoa((temp->client_address).sin_addr), ntohs((temp->client_address).sin_port), ctime(&now), reqbuffer, 404, http_length);
 		}
 		else { //concat with 202.. send input from file to be concatenated
+			
+			
 			fseek(file, 0, SEEK_END);
 			int file_size = ftell(file);
-			char * http = HTTP_200; ///shouldn't be this but i just changed in into this cuz i didn't want error during run.. the program gets hanged so we have to debug it...
+		//	char * http = HTTP_200; ///shouldn't be this but i just changed in into this cuz i didn't want error during run.. the program gets hanged so we have to debug it...
 			//int http_length = strlen(HTTP_200, file_size);
+			char http[128];
+			snprintf(http, 128, HTTP_200, file_size);
+			printf("%s\n", http);
 			int http_length = strlen(http);
-			rewind(file);	
-			char * file_info = malloc(file_size*sizeof(char));
-			fread(file_info, sizeof(char), file_size, file);
+			rewind(file);			
+				
+			char file_info[1024];
+			senddata(temp->sock, http, http_length);
+			while(fread(file_info, sizeof(char), 1024, file)!=0) {
+				senddata(temp->sock, file_info, 1024);
+			}
+			
+		//	fread(file_info, sizeof(char), file_size, file);
 			fclose(file);
 			int total_size = file_size + http_length;
-			char * final_string = malloc(total_size*sizeof(char));
-			strcpy(final_string, http);
-			strcat(final_string, file_info);
+		//	char * final_string = malloc(total_size*sizeof(char));
+		//	strcpy(final_string, http);
+		//	strcat(final_string, file_info);
 			time_t now = time(NULL);
-			senddata(temp->sock, final_string, total_size);
+		//	senddata(temp->sock, final_string, total_size);
 			fprintf(log_file, "%s:%d  %s  'GET %s'  %d %d\n", inet_ntoa((temp->client_address).sin_addr), ntohs((temp->client_address).sin_port), ctime(&now), reqbuffer, 200, total_size);
-			free(file_info);
-			free(final_string);
+		//	free(file_info);
+		//	free(final_string);
 		}
 		free(reqbuffer);
 		//add to output thing
@@ -226,6 +238,7 @@ void runserver(int numthreads, unsigned short serverport) {
        
     }
 	i = 0;
+	pthread_cond_broadcast(&thread); //wake all up
 	for(;i<numthreads; i++) { 
 			printf("in join\n");
     	pthread_join((thread_arr[i]), NULL);
